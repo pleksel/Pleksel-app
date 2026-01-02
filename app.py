4f5afada3a868049902d765be4fcd58ab291cbd4
pleksel <meterikplaats@gmail.com>
2026-01-02 20:14:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-import streamlit as st
 
-tab_data, tab_calc = st.tabs(["Data", "Planning"])
-
-with tab_data:
-    st.write("Dit is de DATA tab")
-
-with tab_calc:
-    st.write("Dit is de PLANNING tab")
 
 
