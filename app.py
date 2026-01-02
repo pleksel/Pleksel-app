a44628ac64b6e8279418b25ace226879b8f64302
pleksel <meterikplaats@gmail.com>
2026-01-02 20:39:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-from fpdf import FPDF
-import io
 import streamlit as st
 # ... andere imports ...
 
 st.session_state.clear() # TIJDELIJK TOEVOEGEN
+
